239250cc3f69be3e29caa1cefa7dbc4c82ce2625
adnanel <adnanel94@gmail.com>
2024-08-25 20:38:39 +0200

    preaccept call

--- html/demos/admin.js
+++ html/demos/admin.js
@@ -5,10 +5,7 @@
 // paths and the like.
 //
 var server = null;
-if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":7088/admin";
-else
-	server = "https://" + window.location.hostname + ":7889/admin";
+server = "http://localhost:7088/admin";
 
 // If you don't want the page to prompt you for a password, insert it here
 var secret = "janusoverlord";
